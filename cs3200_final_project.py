6f65f30e8bba7569bf5270090155934b6cdf33d1
NeonMeduza <85846810+NeonMeduza@users.noreply.github.com>
2022-10-18 22:55:27 -0400

    Discretizing targets and training KNN model

--- cs3200_final_project.py
+++ cs3200_final_project.py
@@ -1,5 +1,13 @@
 import pandas as pd
+import numpy as np
 import os
+from sklearn.preprocessing import LabelEncoder
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import log_loss
+
+'''Set seed'''
+np.random.seed(1)
 
 '''Load the data'''
 train_data = os.getcwd() + "/disease_train_data.csv"
@@ -32,19 +40,50 @@ All columns have integer data type, except for the 'prognosis' columns, which
 is 'object' type or strings in this dataset
 """
 
-'''Inspecting feature scales/values?'''
+'''Inspecting feature scales/values'''
 x = 0
-y = 0
-scale_set = []
+scale_dict = {}
 while (x < len(train_data.columns)-1):
     scale = (train_data.iloc[:, x].unique())
     scale = list(scale)
-    if scale not in scale_set:
-        scale_set.append(scale)
+    keys = list(scale_dict.keys())
+    if str(scale) not in keys:
+        scale_dict[str(scale)] = [train_data.columns[x]]
+    else:
+        scale_dict[str(scale)] += [train_data.columns[x]]
     x = x+1
     
-print(scale_set)
+print(scale_dict)
     
 #Looks like the scale is 0-1, with some features having only 0 values
 
 '''Checking features with only 0 values'''
+print("\n")
+print(scale_dict['[0]'])
+
+'''
+It looks like none of the diseases correspond to the 'fluid_overload'
+symptom; what can we do with it?
+'''
+
+
+'''Meanwhile, we can encode the targets into more discrete values'''
+encoder = LabelEncoder()
+train_data['prognosis'] = encoder.fit_transform(train_data['prognosis'])
+test_data['prognosis'] = encoder.fit_transform(test_data['prognosis'])
+
+
+'''Let us now try some testing on KNN model'''
+#Split data
+x = train_data.iloc[:, :-1]
+y = train_data.iloc[:, -1]
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1,
+                                                    random_state=1)
+
+
+#Create model and train
+model = KNeighborsClassifier(n_neighbors = 1)
+model.fit(x_train, y_train)
+preds = model.predict(x_test)
+#FIXME number of classes between preds and ground truth not equal
+print(log_loss(y_test, preds))
