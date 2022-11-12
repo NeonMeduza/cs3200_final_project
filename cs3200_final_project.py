bfb9b8a9c619509c6fcdce78c22b6ae0899eab85
Dylan M. Van Horn <dmvanho@bgsu.edu>
2022-11-12 18:25:40 -0500

    Added draft for validation method

--- cs3200_final_project.py
+++ cs3200_final_project.py
@@ -7,6 +7,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.feature_selection import VarianceThreshold
+from sklearn.decomposition import PCA
 
 '''Set seed'''
 np.random.seed(1)
@@ -19,26 +20,15 @@ train_data = pd.read_csv(train_data)
 test_data = pd.read_csv(test_data)
 
 '''Searching for NA values and modifying or dropping the values'''
-#print(train_data.columns[train_data.isnull().any()])
-#print(test_data.columns[test_data.isnull().any()])
-
-#A training column is NA, let us check the values
-#print(train_data.loc[0:20, 'Unnamed: 133'])
 
 #The column has only NA values, so it must be removed
 train_data.drop(train_data.columns[133], axis=1, inplace=True)
 print(len(train_data))
 print(len(test_data))
 
-#print(test_data.isnull())
-#print(test_data.keys())
-#print(train_data.keys())
-#print(len(train_data.columns))
-#print(len(test_data.columns))
-#train_data.fillna(//insert method here for replacing na values)
-
 '''Inspecting feature types'''
 #print(train_data.dtypes.value_counts())
+
 """
 All columns have integer data type, except for the 'prognosis' columns, which
 is 'object' type or strings in this dataset
@@ -72,12 +62,9 @@ symptom; what can we do with it?
 
 
 '''Meanwhile, we can encode the targets into more discrete values'''
-#print("orig unique targets: ", np.unique(train_data['prognosis']))
-#print("orig unique targets count: ", len((np.unique(train_data['prognosis']))))
 encoder = LabelEncoder()
 train_data['prognosis'] = encoder.fit_transform(train_data['prognosis'])
 test_data['prognosis'] = encoder.fit_transform(test_data['prognosis'])
-#print("unique targets: ", np.unique(train_data['prognosis']))
 
 
 '''Let us now try some testing on KNN model'''
@@ -88,69 +75,118 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1,
                                                     random_state=1)
 
 #Detect important features
-var_thres = VarianceThreshold(threshold=0.1)
-var_thres.fit(x_train)
-arr = var_thres.get_support()
-opt_feat = list(encoder.fit_transform(arr))
-print(opt_feat)
-
-#filter out unwanted features by adding good features to new dataframe
-print(len(x_train.columns))
-i = 0
-j = 0
-x_train_new = pd.DataFrame()
-x_test_new = pd.DataFrame()
-while (i < len(opt_feat)):
-    if(opt_feat[i] == 1):
-        #pd.concat([x_train_new, x_train], axis=1)
-        #x_train.drop(x_train.columns[i], axis=1, inplace=True)
-        #print(len(x_train.columns))
-        x_train_new.insert(j, x_train.columns[i], x_train.loc[:, x_train.columns[i]], False)
-        x_test_new.insert(j, x_test.columns[i], x_test.loc[:, x_test.columns[i]], False)
-        j = j+1
-    i = i+1
-print("num_features:", len(x_train_new.columns))
-'''
-#Create model and train iteratively
-neighbor_cnt = 1
-neighbor_dict = {}
-while (neighbor_cnt < 300):
-    model = KNeighborsClassifier(n_neighbors=neighbor_cnt)
-    model.fit(x_train_new, y_train)
-    preds = model.predict(x_test_new)
-    neighbor_dict[neighbor_cnt] = (accuracy_score(preds, y_test) * 100)
-    neighbor_cnt += 1
-#print(neighbor_dict.keys())
-#print(neighbor_dict.values())
+def var_data(x_train, x_test, thresh):
+    var_thres = VarianceThreshold(threshold=thresh)
+    var_thres.fit(x_train)
+    arr = var_thres.get_support()
+    opt_feat = list(encoder.fit_transform(arr))
+    print(opt_feat)
     
-#FIXME number of classes between preds and ground truth not equal
-#print("Error score: ", (1 - accuracy_score(preds, y_test)) * 100)
+    #filter out unwanted features by adding good features to new dataframe
+    i = 0
+    j = 0
+    x_train_new = pd.DataFrame()
+    x_test_new = pd.DataFrame()
+    while (i < len(opt_feat)):
+        if(opt_feat[i] == 1):
+            x_train_new.insert(j, x_train.columns[i], x_train.loc[:, x_train.columns[i]], False)
+            x_test_new.insert(j, x_test.columns[i], x_test.loc[:, x_test.columns[i]], False)
+            j = j+1
+        i = i+1
+    return x_train_new, x_test_new
+#print("num_features:", len(x_train_new.columns))
+'''
+#Detect important features via PCA since it is unsupervised
+x_sums = [0 for _ in range(0, len(x.columns))]
+x_means = []
+col_len = len(x.columns)
+row_len = len(x)
+for i in range(0, col_len):
+    for j in range(0, row_len):
+        x_sums[i] += x.values[j][i]
 
+    x_means.append(x_sums[i]/row_len)
+
+
+x_c = x.values - x_means
+
+dim_reduce = PCA(n_components=20)
+dim_reduce.fit(x_c)
+x_transform = dim_reduce.transform(x.values)
+print(type(x_transform))
+x_train_pca = x_transform[:int(len(x_transform)*0.9), :]
+x_test_pca = x_transform[int(len(x_transform)*0.9):, :]
+print(x_test_pca)
+noneCnt = 0
+for i in range(0, len(x_test_pca)):
+    for j in range(0, len(x_test_pca[i])):
+        if (type(x_test_pca[i][j]) == 'NoneType'):
+            noneCnt += 1
+print(noneCnt)
+print(type(x_test_pca[472][9]))
+print(type(x_test_pca[472]))
+print(type(x_test_pca))
+x_test_pca = pd.DataFrame(x_test_pca)
+print(type(x_test_pca))
+'''
+
+#Create model and train iteratively
+def model_train(X_train, X_test):
+    neighbor_cnt = 1
+    neighbor_dict = {}
+    for neighbor_cnt in range(1, 301):
+        model = KNeighborsClassifier(n_neighbors=neighbor_cnt)
+        model.fit(X_train, y_train)
+        preds = model.predict(X_test)
+        neighbor_dict[neighbor_cnt] = (accuracy_score(preds, y_test) * 100)
+    return neighbor_dict
+
+'''
 #Plot for elbow method
 plt.title("Accuracy with varying neighbors")
 #plt.invert_xaxis()
-plt.plot(neighbor_dict.keys(), neighbor_dict.values())
-
+plt.plot(unvar_neighbor_dict.keys(), unvar_neighbor_dict.values())
+'''
 #Variance might be high here; decrease neighbors until accuracy is <95%
-def filtering(x):
+def filtering(dictionary):
     i = 1
     optimal_k = 0
     maxK = 0
-    while(i <= len(x)):
-        if (x[i] > maxK):
+    while(i <= len(dictionary)):
+        if (dictionary[i] > maxK):
             optimal_k = i
-            maxK = x[i]
+            maxK = dictionary[i]
         i = i+1
     return optimal_k
-
-opt_k = filtering(neighbor_dict)
-print("num_neighbors:", opt_k)
-print("accuracy:", neighbor_dict[opt_k])
 '''
+neighbor_dict1 = model_train(x_train, x_test)
+opt_k1 = filtering(neighbor_dict1)
+print("num_neighbors:", opt_k1)
+print("accuracy:", neighbor_dict1[opt_k1])
+'''
+new_x_train, new_x_test = var_data(x_train, x_test, 0.1)
+neighbor_dict2 = model_train(new_x_train, new_x_test)
+opt_k2 = filtering(neighbor_dict2)
+print("num_neighbors2: ", opt_k2)
+print("accuracy2: ", neighbor_dict2[opt_k2])
+
+new_x_train, new_x_test = var_data(x_train, x_test, 0.05)
+neighbor_dict3 = model_train(new_x_train, new_x_test)
+opt_k3 = filtering(neighbor_dict3)
+print("num_neighbors3: ", opt_k3)
+print("accuracy3: ", neighbor_dict3[opt_k3])
+"""More features there are, less neighbors are needed and higher accuracy is
+achieved, which explains why having all features present warrants perfect
+accuracy for the first couple hundred neighbors. However, this high accuracy
+with low neighbors could indicate overfitting, so validation is needed to see
+if variance is high. Also, the weights of the model need to be compared with
+the features deemed important by the variance threshold to ensure the model
+does not have a bias unequal to the actually important features"""
 
 '''
 The model is tested on other datasets to determine how well it generalizes,
 based on its current accuracy to see what neighbor and features counts are best
+'''
 '''
 blocks = 2
 test_scores = {}
@@ -194,7 +230,7 @@ while blocks < 5:
         tests = tests+1
         
     blocks = blocks+1
-
+'''
 '''
 splits = 15
 K = 2
